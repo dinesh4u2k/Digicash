9e715bb092d2e1b6462b7263a7ef21bf36326997
dinesh4u2k <dinesh4u2k@gmail.com>
2018-07-18 20:08:51 +0530

    Update MyService.java

--- app/src/main/java/com/example/batman/digicash1/MyService.java
+++ app/src/main/java/com/example/batman/digicash1/MyService.java
@@ -22,5 +22,39 @@ public class MyService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MyService.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MyService.this,
+                    Manifest.permission.READ_PHONE_STATE)){
+                ActivityCompat.requestPermissions(MyService.this, new String[]{
+                        Manifest.permission.READ_PHONE_STATE}, 1);
+
+            } else {
+
+                ActivityCompat.requestPermissions(MyService.this, new String[]{
+                        Manifest.permission.READ_PHONE_STATE}, 1);
+
+            }
+        } else {
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MyService.this,
+                            Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "No permission Granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 }
